82b2c5d5ff8f5fb8c99ceb9ac84563a8ddd269d1
accelthreat <matthewlemuelr@gmail.com>
2016-08-01 17:28:09 +0800

    Changed delay

--- Piezzo.cpp
+++ Piezzo.cpp
@@ -6,22 +6,22 @@
 
 #include "Piezzo.h"
 
-Piezzo::Piezzo (int piezzopinz){
+Piezzo::Piezzo (int piezzopinz) {
   piezzoPin = piezzopinz;
   pinMode(piezzoPin, OUTPUT);
 
 }
 
 void Piezzo::playNote (int freq, int duration) {
-tone (piezzoPin,freq,duration);
-delay(50+duration);
-  }
+  tone (piezzoPin, freq, duration);
+  delay(1 + duration);
+
+}
 
 
 void Piezzo::play (int notes [], int noteLength) {
-for (int i=0; i<noteLength; i+=2) {
+  for (int i = 0; i < noteLength; i += 2) {
+    playNote (notes [i], notes[i + 1]);
 
-    playNote (notes [i], notes[i+1]);
-
-}
+  }
 }
